ca5c7b5ac327f428ccd1809a022ec6e2de9fb8e0
Siklodi Mark <smark.siklodi@gmail.com>
2022-07-17 18:02:25 +0200

    'universalnavbar'

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,19 +9,46 @@ const NavLink = ({to, children}) => {
     </Link>
 }
 
+const NavBarMode = ({ stylemode, onClick }) => {
+
+    const desktopMode = "mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:bg-cyan-600 duration-200 text-white font-semibold py-2 px-4"
+    const mobileMode = "text-3xl text-center font-semibold my-2 hover:bg-slate-700 w-56 rounded-full"
+
+    switch (stylemode) {
+        case 'mobile':
+            stylemode = mobileMode
+            break;
+        case 'desktop':
+            stylemode = desktopMode
+            break;
+    
+        default:
+            break;
+    }
+
+    return (
+        <>
+            <NavLink to="/works">
+                    <button className={stylemode} onClick={onClick}>Works</button>
+            </NavLink>
+            <NavLink to="/about">
+                    <button className={stylemode} onClick={onClick}>About me</button>
+            </NavLink>
+            <NavLink to="/contact">
+                    <button className={stylemode} onClick={onClick}>Contact</button>
+            </NavLink>
+        </>
+    )
+}
+
 const MobileNav = ({open, setOpen}) => {
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen  bg-slate-800 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
-            <div className="flex items-center justify-center filter bg-slate-800 h-20 md:hidden"> {/*logo container*/}
-            <NavLink to="/"><Image className="text-2xl font-semibold" src="/logo.svg" href="/" width={80} height={80}/></NavLink>
+            <div className="flex items-center justify-center filter bg-slate-900 h-20 md:hidden"> {/*logo container*/}
+            <NavLink to="/"><Image className="text-2xl font-semibold" src="/logo.svg" href="/" width={80} height={80} onClick={() => setTimeout(() => {setOpen(!open)}, 100)}/></NavLink>
             </div>
-            <div className="flex flex-col ml-4">
-                <a className="text-3xl text-center font-semibold my-2 hover:bg-slate-700 w-56 rounded-full" href="/about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    About me
-                </a>
-                <a className="text-3xl text-center font-semibold my-2 hover:bg-slate-700 w-56 rounded-full" href="/contact" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Contact
-                </a>
+            <div className="flex flex-col ml-4 py-16">
+                <NavBarMode stylemode="mobile" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}/>
             </div>  
         </div>
     )
@@ -38,25 +65,15 @@ const Navbar = () => {
             </div>
             <div className="w-9/12 flex justify-end items-center">
 
-                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
-                    setOpen(!open)
-                }}>
+                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {setOpen(!open)}}>
                     {/* hamburger button */}
-                    <span className={`h-2 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-                    <span className={`h-2 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-                    <span className={`h-2 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                    <span className={`h-2 w-full bg-cyan-500 shadow-lg shadow-cyan-500/80 rounded-lg transform transition duration-200 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-2 w-full bg-cyan-500 shadow-lg shadow-cyan-500/80 rounded-lg transition-all duration-200 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-2 w-full bg-cyan-500 shadow-lg shadow-cyan-500/80 rounded-lg transform transition duration-200 ease-in-out ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
                 </div>
 
                 <div className="hidden md:flex">
-                    <NavLink to="/works">
-                        <button className='mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:bg-cyan-600 duration-200 text-white font-semibold py-2 px-4 '>Works</button>
-                    </NavLink>
-                    <NavLink to="/about">
-                        <button className='mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:bg-cyan-600 duration-200 text-white font-semibold py-2 px-4 '>About me</button>
-                    </NavLink>
-                    <NavLink to="/contact">
-                        <button className='mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:bg-cyan-600 duration-200 text-white font-semibold py-2 px-4 '>Contact</button>
-                    </NavLink>
+                <NavBarMode stylemode="desktop" />
                 </div>
             </div>
         </nav>
--- pages/works.jsx
+++ pages/works.jsx
@@ -2,7 +2,11 @@ import { motion } from "framer-motion"
 
 const Works = () => {
       return ( 
-            
+      <div>
+            <h1 className='font-bold text-5xl pb-5'>  Works </h1>
+            <p>   This is the main page of the web application.   </p>
+            <p>   You can find the documentation on how to use this site.     </p>
+      </div> 
        );
 }
  
