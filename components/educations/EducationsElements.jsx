bce6e4a0fd2516ad7d051e04fb1b6b24ae7027dc
Siklodi Mark <smark.siklodi@gmail.com>
2022-07-25 15:27:59 +0200

    flex-auto work/education

--- components/educations/EducationsElements.jsx
+++ components/educations/EducationsElements.jsx
@@ -2,7 +2,7 @@ import { educationListElements_HU, educationListElements_ENG } from "../../store
 
 export const educationElements =  educationListElements_HU.map((education) => {
       return (
-                          <div className="relative cursor-pointer inline-flex flex-col border-cyan-900 bg-cyan-900/30 hover:bg-teal-800/30 hover:border-teal-500/50 hover:shadow-teal-500/40 shadow-xl shadow-black-500/80 rounded-3xl mx-5 md:mx-5 w-96 md:w-80 h-80 mb-10">
+                          <div className="relative cursor-pointer inline-flex flex-col border-cyan-900 bg-cyan-900/30 hover:bg-teal-800/30 hover:border-teal-500/50 hover:shadow-teal-500/40 shadow-xl shadow-black-500/80 rounded-3xl mx-5 w-96 md:flex-auto h-80 mb-10">
                               <h1 className=" text-3xl justify-end font-bold bg-blue-800/30 w-full rounded-t-2xl">{education.start} <strong>-</strong> {education.end}</h1>     
                             <div className="flex flex-col items-center justify-center">
                               <div className="text-lg py-10 pb-10">
--- components/works/WorksElements.jsx
+++ components/works/WorksElements.jsx
@@ -2,7 +2,7 @@ import { workListElements_HU, workListElements_ENG } from "../../store/works_dat
 
 export const WorkElements =  workListElements_HU.map((work) => {
       return (
-        <div className="relative cursor-pointer inline-flex flex-col border-cyan-900 bg-cyan-900/30 hover:bg-teal-800/30 hover:border-teal-500/50 hover:shadow-teal-500/40 shadow-xl shadow-black-500/80 rounded-3xl mx-5 md:mx-5 w-96 md:w-80 h-56 mb-10">
+        <div className="relative cursor-pointer inline-flex flex-col border-cyan-900 bg-cyan-900/30 hover:bg-teal-800/30 hover:border-teal-500/50 hover:shadow-teal-500/40 shadow-xl shadow-black-500/80 rounded-3xl mx-5 w-96 md:flex-auto h-56 mb-10">
               <p className="text-3xl justify-end font-bold bg-blue-800/30 w-full rounded-t-2xl">{work.start} <strong>-</strong> {work.end}</p>                                  
           <div className="flex flex-col items-center justify-center">
               <h1 className="text-2xl font-bold py-10 pb-10 ">{work.position}</h1>
