739b98d6702ab22675e33bf206e592e6b99da97e
Siklodi Mark <smark.siklodi@gmail.com>
2022-07-31 18:43:58 +0200

    'home'

--- components/home/HomeComp.jsx
+++ components/home/HomeComp.jsx
@@ -11,6 +11,7 @@ const HomeComp = () => {
                   <h1 className='font-bold'>{welcome_HU.name}</h1>
                   <h2 className='font-semibold text-slate-400'>{currentAge} éves {welcome_HU.profession} vagyok {welcome_HU.liveAt}ról</h2>
                   {/* <Image src={welcome_HU.image} height={400} width={200} /> */}
+                  <a href='./help.pdf' target='_blank' rel='noopener noreferrer' ><button className='mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 duration-200 text-white font-semibold py-2 px-4'>Önéletrajz Letöltés</button></a>
             </div>
        );
 }
