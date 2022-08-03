402a75c67140e7dc15078fc5deb827a04cf90726
Siklodi Mark <smark.siklodi@gmail.com>
2022-08-03 22:17:47 +0200

    welcome home

--- components/home/HomeComp.jsx
+++ components/home/HomeComp.jsx
@@ -24,14 +24,16 @@ const HomeComp = () => {
             )}
 
       return (
-            <div className='align-middle items-center space-y-8'>
-                  <div className='inline-flex space-x-3'>
+            <div className='align-middle items-center'>
+                  <h1 className='lg:text-5xl font-bold pb-10'>Üdvözöllek!</h1>
+                  <div className='inline-flex space-x-3 pb-2'>
                         <h1 className='font-bold'>A nevem</h1>
                         <h1 className='font-bold underline'> {welcome_HU.name}</h1>
                   </div>
-                  <div className='text-xl'>
-                        <p className='font-semibold'>Üdvözöllek a Weboldalomon!</p>
-                        <p className=' text-slate-400 pb-10'>{currentAge} éves {welcome_HU.profession} vagyok {welcome_HU.liveAt}ról</p>
+                  <div className='text-2xl'>
+                        <p className=' text-slate-400 pb-10'>{currentAge} éves {welcome_HU.profession}</p>
+                  </div>
+                  <div>
                         <Image src={welcome_HU.image} height={300} width={300} className="rounded-full hover:ring-1 duration-200 ring-teal-300"/> 
                         {/* <button className='mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 duration-200 text-white font-semibold py-2 px-4' onClick={saveFile}>Önéletrajz Letöltés</button> */}
                   </div>
