6ce51e8fcbfb86874485307c46606d0ce20e1607
Siklodi Mark <smark.siklodi@gmail.com>
2023-05-04 11:21:27 +0200

    .

--- components/home/HomeComp.jsx
+++ components/home/HomeComp.jsx
@@ -1,20 +1,19 @@
-import Image from 'next/image';
 import { welcome_HU } from '../../store/welcome_data';
 import { saveAs } from 'file-saver';
-import pdf from './help.pdf';
-
 //import downloadFile from '../../utility/downloadPDF';
 
 const HomeComp = () => {
     const currentYear = new Date().getFullYear();
     const currentAge = currentYear - welcome_HU.birth.substring(0, 4);
 
-    const saveFile = () => {
-        saveAs(pdf, 'help.pdf');
-    };
-
-    const downloadPDF = () => {
-        downloadFile('../../store/files/help.pdf', 'help.pdf');
+    const saveFile = async () => {
+        try {
+            const res = await fetch('/api/download');
+            const blob = await res.blob();
+            saveAs(blob, 'Scridon-Siklodi_Mark_CV.pdf');
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -29,12 +28,16 @@ const HomeComp = () => {
                     {currentAge} éves {welcome_HU.profession}
                 </p>
             </div>
-            <div className="">
+            <div className="flex flex-col gap-10">
                 <img
                     src={welcome_HU.image}
                     className="object-cover w-80 h-60 lg:w-96 lg:h-72 object-right rounded-full mx-auto border-4 border-cyan-900"
                 />
-                {/* <button className='mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 duration-200 text-white font-semibold py-2 px-4' onClick={saveFile}>Önéletrajz Letöltés</button> */}
+                <button
+                    className="w-60 text-md lg:text-lg mx-auto rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 duration-200 text-white font-semibold py-2 px-4"
+                    onClick={saveFile}>
+                    Önéletrajz Letöltés
+                </button>
             </div>
         </div>
     );
