08500da407ef88a8076f452796fbded89478894f
Siklodi Mark <smark.siklodi@gmail.com>
2023-05-04 11:53:57 +0200

    .

--- components/navbar/NavBarMode.jsx
+++ components/navbar/NavBarMode.jsx
@@ -4,7 +4,7 @@ const NavBarMode = ({ stylemode, onClick, open }) => {
     const desktopMode =
         'mx-4 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 duration-200 text-white font-semibold py-2 px-4';
     const mobileMode =
-        'text-lg text-center ml-2 text-white w-32 h-10 font-semibold my-2 bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 rounded-xl overflow-y-hidden';
+        'text-sm md:text-xl text-center ml-2 text-white w-[30%] h-10 font-semibold my-2 bg-cyan-500 shadow-lg shadow-cyan-500/30 hover:bg-teal-600 hover:shadow-teal-700 rounded-xl overflow-y-hidden';
 
     switch (stylemode) {
         case 'mobile':
